0cd87f43966a941268edfd11ebeaa19c180e6b5b
kaypappi <wesleyukadike@yahoo.com>
2019-09-26 04:07:07 +0100

    Updates

---
+++ precache-manifest.658b729ecec6ce346e6e269bd9a79f47.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce7fb6602093f39bc5635346ebbb871",
+    "url": "/Opay-demo/index.html"
+  },
+  {
+    "revision": "ca651e1e2a8778204449",
+    "url": "/Opay-demo/static/css/2.7d36abf2.chunk.css"
+  },
+  {
+    "revision": "ca2a7b63f4c42c59aa5b",
+    "url": "/Opay-demo/static/css/main.ce5dea9c.chunk.css"
+  },
+  {
+    "revision": "ca651e1e2a8778204449",
+    "url": "/Opay-demo/static/js/2.bee8427f.chunk.js"
+  },
+  {
+    "revision": "ca2a7b63f4c42c59aa5b",
+    "url": "/Opay-demo/static/js/main.654ecf2c.chunk.js"
+  },
+  {
+    "revision": "ca8a4359c17f82272c1f",
+    "url": "/Opay-demo/static/js/runtime-main.6f01071c.js"
+  },
+  {
+    "revision": "ca4d8a2c1c0f23a9ba49599c68a2ba7c",
+    "url": "/Opay-demo/static/media/LinkedIn.ca4d8a2c.svg"
+  },
+  {
+    "revision": "066b3859ecb46798292ea1d26a967dd4",
+    "url": "/Opay-demo/static/media/bar.066b3859.png"
+  },
+  {
+    "revision": "1bc8d20ead78b9334aaf489cfd974159",
+    "url": "/Opay-demo/static/media/building.1bc8d20e.svg"
+  },
+  {
+    "revision": "b579f788a63f08f83c6fd89ecffb82e4",
+    "url": "/Opay-demo/static/media/curves1.b579f788.png"
+  },
+  {
+    "revision": "23c7e46a7a719003aa54465a800ea956",
+    "url": "/Opay-demo/static/media/curves4.23c7e46a.png"
+  },
+  {
+    "revision": "3f1e20f6e618249ab1ed1a5f4bf04a36",
+    "url": "/Opay-demo/static/media/facebook.3f1e20f6.svg"
+  },
+  {
+    "revision": "6b2a56234da3badbcaac27068b6d79c0",
+    "url": "/Opay-demo/static/media/instagram.6b2a5623.svg"
+  },
+  {
+    "revision": "694988dcc2df939446be136fd367f079",
+    "url": "/Opay-demo/static/media/jaime.694988dc.png"
+  },
+  {
+    "revision": "51f8f52c0d18c6d30b3c6e40b0d3e4a3",
+    "url": "/Opay-demo/static/media/menu.51f8f52c.svg"
+  },
+  {
+    "revision": "196a8bc3e4b4c134929864bf9ed70503",
+    "url": "/Opay-demo/static/media/phone.196a8bc3.png"
+  },
+  {
+    "revision": "78986dc8f536baf3c233a298bb6fbf0b",
+    "url": "/Opay-demo/static/media/phone.78986dc8.svg"
+  },
+  {
+    "revision": "ce081870e8e897d8fba597164e3f8f7a",
+    "url": "/Opay-demo/static/media/piggy.ce081870.svg"
+  },
+  {
+    "revision": "36f1084bfc24e21ee9d4cbf3f656e34c",
+    "url": "/Opay-demo/static/media/playstore.36f1084b.svg"
+  },
+  {
+    "revision": "a40607c3e7bbef9a99951b586b0256e3",
+    "url": "/Opay-demo/static/media/shield.a40607c3.svg"
+  },
+  {
+    "revision": "9c70320f2614761a0f457c077f243c93",
+    "url": "/Opay-demo/static/media/shuttle.9c70320f.svg"
+  },
+  {
+    "revision": "326db2a694394c4e450c96eb159f8423",
+    "url": "/Opay-demo/static/media/slack.326db2a6.svg"
+  },
+  {
+    "revision": "380a1bea53c7937dec5e2647ac82cb8c",
+    "url": "/Opay-demo/static/media/smallphone.380a1bea.png"
+  },
+  {
+    "revision": "4dd81faaf8a8f3906f1536f8aa75c1f1",
+    "url": "/Opay-demo/static/media/tag.4dd81faa.svg"
+  },
+  {
+    "revision": "ce2fb46ef219e5c60f82f6564177f8cb",
+    "url": "/Opay-demo/static/media/twitter.ce2fb46e.svg"
+  }
+]);
